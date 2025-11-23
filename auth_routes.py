9e44d49c6ebb81052349f494e27e34db18a10848
jordana amorim <jordana_amorim@hotmail.com>
2025-11-23 18:46:19 -0300

    create register_user route

--- auth_routes.py
+++ auth_routes.py
@@ -1,5 +1,10 @@
+from models import User
 from http import HTTPStatus
 from fastapi import APIRouter
+from fastapi.params import Depends
+
+from dependencies import catch_session
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -9,3 +14,15 @@ async def authentication():
     return {"message": "Authentication successful"}
 
 
+@auth_router.post("/register_user")
+async def register_user(email: str, name: str, password: str, session=Depends(catch_session)):
+    """Register a new user."""
+    usuario = session.query(User).filter(User.email == email).first()
+    if usuario:
+        return {"message": "User already exists"}
+    else:
+        hashed_password = bcrypt_context.hash(password)
+        new_user = User(name=name, email=email, password=hashed_password)
+        session.add(new_user)
+        session.commit() 
+    return {"message": "User registered successfully"}
